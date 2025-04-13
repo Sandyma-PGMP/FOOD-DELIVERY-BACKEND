30ad8d6e8d92cc9cebe5345447ae3a06b6ef51ef
Santhosh <santhosharumugam2k3@gmail.com>
2025-04-13 09:35:41 +0530

    Updated CORS

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://food-delivery-frontend-tau.vercel.app",
+    origin: "https://food-delivery-frontend-te9b.vercel.app",
     methods: ["GET", "POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
